5f5725ef3ba30e6e43629bd3ed9e6ec0e9089775
Yogesh Manni <yogeshmanni786@gmail.com>
2023-10-31 23:35:42 -0700

    Removed unnecessary code from login.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php 
  require "functions.php";
  require "Database.php";
- //require "router.php";
-$config = require('config.php');
-
-$db = new Database($config,'root',$config['password']);
-
-
-$note =$db->query("select * from users")->fetchAll();
-
-var_dump($note);
-
+  require "router.php";
 
 ?>
