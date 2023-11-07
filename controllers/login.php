f9a2cd173c108e43a2b00c88008f799e54b54228
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-06 17:25:50 -0800

    Added mysql query to validate user's login credentials

--- controllers/login.php
+++ controllers/login.php
@@ -1,5 +1,29 @@
 <?php
+ $heading = "Login";
+ 
+ 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_REQUEST['email']))
+{
+      require_once "Database.php";  
+      $user = $_REQUEST;
+      $errors = null;
+      $query = "select * from users where email = :email";         
+      $res = Database::db()->query($query,["email" => $_REQUEST['email']])->fetch();
+        
+      if($res == false)
+      {
+        $errors = "User email not found, please check again or register a new account !!";
+      }
+      else if($res['password'] == $_REQUEST['password'])
+        {
+                header("Location:$_SERVER[HTTP_REFERER]home");
+        }
+        else
+        {
+                $errors = "Password Incorrect, please try again !!";
+       }
+        
+}
+      require("views/login.view.php");
 
-        $heading = "Login";
-       require "views/login.view.php";
 ?>
--- views/login.view.php
+++ views/login.view.php
@@ -13,7 +13,7 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="../controllers/login.php" method="POST">
+    <form class="space-y-6"  method="POST">
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <div class="mt-2">
@@ -37,7 +37,8 @@
         <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
       </div>
     </form>
-
+  <p style='color:red'><?=$errors ?? ''?></p>
+    
     <p class="mt-10 text-center text-sm text-gray-500">
       Not have a login?
       <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register Now</a>
