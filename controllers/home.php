03064ba1761630adeae11af95bdb8f7157002637
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-06 18:40:25 -0800

    Fetched list of services from database in home controller

--- controllers/home.php
+++ controllers/home.php
@@ -2,8 +2,8 @@
 
 
         $heading = "Home";
-       
-      
+        require_once "Database.php"; 
+       $services = Database::db()->query("select * from services")->fetchAll();   
        require "views/home.view.php";
 
 ?>
