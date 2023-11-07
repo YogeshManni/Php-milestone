9c2befb2accfcca6c8b9ac6a0151b96e18dfa349
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-06 17:18:38 -0800

    Added additional check in register controller to validate incoming data

--- controllers/register.php
+++ controllers/register.php
@@ -1,22 +1,38 @@
 <?php 
-
+ $heading = "Register";
 
 if (isset($_REQUEST['username']))
 {
-    require "../Database.php"; 
-    $heading = "Register";
-    $user = $_REQUEST;
-    $query = "INSERT INTO users (username, email, password)
-     VALUES ('{$user['username']}','{$user['email']}','{$user['password']}')";
-             
-    $note = Database::db()->query($query);
-    var_dump($note);
-}
-else
-{
 
-require "views/register.view.php";
+    require_once "Database.php"; 
+    $errors = null;
+    $success = null;
+    $user = $_REQUEST;
+    $checkUser =  "select * from users where email = :email";   
+    $res = Database::db()->query($checkUser,["email" => $_REQUEST['email']])->fetch();   
+    if($res == false)
+    {
+        $query = "INSERT INTO users (username, email, password) VALUES ('{$user['username']}','{$user['email']}','{$user['password']}')";
+        try
+        {
+            $res = Database::db()->query($query);
+            $success = "User successfully registered, please proceed to login!!";
+        }
+        catch (Exception $e)
+        {
+            $errors = "Error occured, please try again!!";
+        }
+    }
+    else
+    {
+            $errors = "Email already exists, please login or use different email!!";
+    }
+  
+    
 }
+
+
+require("views/register.view.php");
 
 
 ?>
