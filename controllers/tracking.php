a41f4b2ef6c377a61c8a44981a4bc28e0a141431
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-07 22:31:14 -0800

    for tracking created a unique order number for each service order sand pushed it into database

--- controllers/tracking.php
+++ controllers/tracking.php
@@ -1,6 +1,22 @@
 <?php 
 
 $heading = "Track services";
+
+$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+$order_number =  substr(str_shuffle($characters), 0, 6);
+
+require_once "Database.php"; 
+$query = "Insert into service_status(status, order_number, user_id) values('Service Ordered',:order_number,1)";
+try
+{
+    Database::db()->query($query,['order_number' => $order_number]);   
+}
+catch(Exception $ex)
+{
+    echo ''. $ex->getMessage();
+}
+
 require "views/tracking.view.php"
 
 
