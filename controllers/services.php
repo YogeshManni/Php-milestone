e4ba8d27bc210ca7fc2e074e5ecd952cdf58b31d
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-07 22:20:11 -0800

    Fetched the list of services from database

--- controllers/services.php
+++ controllers/services.php
@@ -1,6 +1,10 @@
 <?php 
 
 $heading = "Our services";
+require_once "Database.php"; 
+$id = $_GET['id'] ?? 1;
+$service_detail = Database::db()->query("select * from service_details where service_id=:id",['id'=>$id])->fetch();   
+
 require "views/services.view.php"
 
 
