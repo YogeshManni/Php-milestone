151e225556f7c98962e0a3138158a58179561348
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-07 22:51:58 -0800

    Comments added for services page

--- controllers/services.php
+++ controllers/services.php
@@ -1,4 +1,8 @@
 <?php 
+/*
+    Service id will be fetched from the current toute and will be used to
+    fetched service detail from the database
+*/
 
 $heading = "Our services";
 require_once "Database.php"; 
