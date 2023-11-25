d497e068f748377e5e90c46c6e9b5758d3ae03a4
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-24 16:19:04 -0800

    created a global proj_root variable and getRoot function to access root path

--- milestone3/functions.php
+++ milestone3/functions.php
@@ -1,6 +1,18 @@
 <?php
+
+ global $proj_root;
+ $proj_root = "/milestone3";
  function urlIs($value)
  {
-      return $_SERVER['REQUEST_URI'] === $value;
+     
+     global $proj_root;
+     
+     return $proj_root.$_SERVER['REQUEST_URI'] === $value;
  } 
+
+ function getRoot()
+ {
+     global $proj_root;
+     return $proj_root;
+ }
 ?>
