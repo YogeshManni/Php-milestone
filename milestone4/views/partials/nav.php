f093d4e842d4e0d99a5155214279bc56d03e672b
Yogesh Manni <yogeshmanni786@gmail.com>
2023-11-26 17:54:59 -0800

    Added Orders menu in navbar and added a check so only admin can see it

--- milestone4/views/partials/nav.php
+++ milestone4/views/partials/nav.php
@@ -26,7 +26,12 @@
                         <a href="tracking"
                             class="<?= urlIs('/tracking') ?  "bg-gray-900 text-red-500" : "text-gray-300"; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
                             Track Services</a>
-
+                        <?php if($_SESSION['username'] == 'admin')
+                        { ?>
+                         <a href="orders"
+                            class="<?= urlIs('/orders') ?  "bg-gray-900 text-red-500" : "text-gray-300"; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                            Orders</a>  
+                        <?php } ?> 
                     </div>
                 </div>
             </div>
